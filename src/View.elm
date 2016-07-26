ecc93d96b65dcc295ab1416117c7002fe589a8b6
Nikita Tchayka <nikitatchayka@gmail.com>
2016-07-26 21:19:40 +0100

    Make a hello world

--- src/View.elm
+++ src/View.elm
@@ -2,6 +2,13 @@ module View exposing (view)
 import Model exposing (Model)
 import Update exposing (Msg(..))
 import Html exposing (..)
+import Html.Attributes exposing (..)
 
 view : Model -> Html Msg
-view model = text "Hello"
+view model =
+    div
+    [ class "ui middle aligned center aligned grid" ]
+    [ div
+      [ class "column" ]
+      [ h1 [ class "ui teal header" ] [ text "Hello world!" ] ]
+    ]
